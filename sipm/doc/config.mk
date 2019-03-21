43466abad3f824c7f526c6082e58c3e8f694552d
Luca Tosti <luca.tosti92@gmail.com>
2019-03-21 12:29:05 +0100

    compiling and linking done

--- sipm/doc/config.mk
+++ sipm/doc/config.mk
@@ -16,7 +16,7 @@ SRC_PATH = ./src
 LIBS =
 
 # General compiler flags
-COMPILE_FLAGS = -std=c++11 -Wall -Wextra -g
+COMPILE_FLAGS = -std=c++11 -Wall -Wextra -g -fPIC  
 
 # Additional release-specific flags
 RCOMPILE_FLAGS = -D NDEBUG
